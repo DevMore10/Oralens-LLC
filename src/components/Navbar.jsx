4c178eca043bbe0bd8a4f70eb9bcde366b1c690a
Dev More <devmore.2004@gmail.com>
2024-11-19 01:24:30 +0530

    html changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav className=" p-3 px-5 bg-[#B9E5E8] border-b-[#4A628A] border-b-4">
-      <div className="container p-0 mx-auto flex justify-between items-center">
+      <div className="container p-0 mx-auto flex  items-center">
         <Link to="/">
           <img
             className="cursor-pointer"
